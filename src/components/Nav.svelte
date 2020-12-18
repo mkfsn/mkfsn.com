0669c880fc8f5785e03f5bc8f53205174c920449
Pei-Ming Wu <p408865@gmail.com>
2020-12-18 23:43:25 +0800

    feat: add hover style

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,9 +1,9 @@
-<nav class="py-2 bg-gray-50">
+<nav class="bg-gray-50">
     <div class="px-4">
         <div class="flex justify-center items-center">
             <div class="flex">
-                <a class="p-2 text-gray-500" href="/blog">Blog</a>
-                <a class="p-2 text-gray-500" href="/resume">Resume</a>
+                <a class="flex-1 px-2 py-4 text-gray-500 hover:bg-gray-300 hover:text-gray-800" href="/blog">Blog</a>
+                <a class="flex-1 px-2 py-4 text-gray-500 hover:bg-gray-300 hover:text-gray-800" href="/resume">Resume</a>
             </div>
         </div>
     </div>
