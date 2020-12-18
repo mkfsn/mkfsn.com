648c71d8be48f0a210bf864737787765e419fbe6
Pei-Ming Wu <p408865@gmail.com>
2020-12-19 00:35:32 +0800

    feat: add social media icons and links

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,4 +1,6 @@
 <script>
+    import Icon from 'svelte-awesome';
+    import { twitterSquare, linkedinSquare, githubSquare, mapMarker } from 'svelte-awesome/icons';
     let this_year = (new Date()).getFullYear();
 </script>
 
@@ -11,11 +13,24 @@
                 <span class="text-xl">aka mkfsn</span>
             </h2>
             <h5>
-                Software Engineer, Taiwan
+                <a class="pl-2" target="_blank" href="https://www.google.com.tw/maps/place/%E5%8F%B0%E7%81%A3/">
+                    <Icon scale="1.5" data={mapMarker}/> Taiwan
+                </a>
             </h5>
         </div>
+        <div class="text-center pt-5">
+            <a class="pl-1" target="_blank" href="//twitter.com/mkfsn">
+                <Icon scale="1.5" data={twitterSquare}/>
+            </a>
+            <a class="pl-1" target="_blank" href="//github.com/mkfsn">
+                <Icon scale="1.5" data={githubSquare}/>
+            </a>
+            <a class="pl-1" target="_blank" href="//linkedin.com/in/mkfsn">
+                <Icon scale="1.5" data={linkedinSquare}/>
+            </a>
+        </div>
     </figcaption>
-    <p class="text-center mt-5 pt-20">&copy; {this_year}</p>
+    <p class="text-center mt-5 pt-12">&copy; {this_year}</p>
 </figure>
 
 <style>
