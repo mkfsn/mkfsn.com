46131bf121497c3bd780ce697880731424120f0d
Pei-Ming Wu <p408865@gmail.com>
2020-12-18 23:44:17 +0800

    fix: broken links

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -2,8 +2,8 @@
     <div class="px-4">
         <div class="flex justify-center items-center">
             <div class="flex">
-                <a class="flex-1 px-2 py-4 text-gray-500 hover:bg-gray-300 hover:text-gray-800" href="/blog">Blog</a>
-                <a class="flex-1 px-2 py-4 text-gray-500 hover:bg-gray-300 hover:text-gray-800" href="/resume">Resume</a>
+                <a class="flex-1 px-2 py-4 text-gray-500 hover:bg-gray-300 hover:text-gray-800" href="//mkfsn.github.io/blog">Blog</a>
+                <a class="flex-1 px-2 py-4 text-gray-500 hover:bg-gray-300 hover:text-gray-800" href="//mkfsn.github.io/resume">Resume</a>
             </div>
         </div>
     </div>
